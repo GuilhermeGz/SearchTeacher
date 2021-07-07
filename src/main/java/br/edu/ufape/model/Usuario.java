d2c9e4de2fe5dd3fc02ab4340372e58a3ade71a7
Guilherme Silva <guilherme.dna06@gmail.com>
2021-07-06 21:18:31 -0300

    Atualizações de atributos do model Usuario

--- src/main/java/br/edu/ufape/model/Usuario.java
+++ src/main/java/br/edu/ufape/model/Usuario.java
@@ -1,11 +1,21 @@
 package br.edu.ufape.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import br.edu.ufape.model.Perfil;
 
 @Entity 
 //Referindo-se a tebela no banco.
@@ -22,7 +32,19 @@ public class Usuario {
 	
 	@NotBlank(message="errou")
 	private String senha;
-
+	
+	private Integer Ativo;
+	
+	 @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	 @JoinTable(
+	            name = "usuario_perfil",
+	            joinColumns = @JoinColumn(name = "idusuario"),
+	            inverseJoinColumns = @JoinColumn(name = "idperfil"))
+	private List<Perfil> perfis = new ArrayList<Perfil>();
+	 
+	
+	// Getters e Setters.
+	 
 	public Long getId() {
 		return id;
 	}
@@ -47,6 +69,21 @@ public class Usuario {
 		this.senha = senha;
 	}
 
+	public List<Perfil> getPerfis() {
+		return perfis;
+	}
+
+	public void setPerfis(List<Perfil> perfis) {
+		this.perfis = perfis;
+	}
+
+	public Integer getAtivo() {
+		return Ativo;
+	}
+
+	public void setAtivo(Integer ativo) {
+		Ativo = ativo;
+	}
 
 
 
